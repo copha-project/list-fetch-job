db71e8f6a5ba11126dbf2d44ea6d3382bdd730ae
aong <yxwzaxns@gmail.com>
2021-09-13 17:29:51 +0800

    update project structure

--- src/index.js
+++ src/index.js
@@ -135,11 +135,12 @@ class ListJob extends Job {
                 continue
             }
             //是否已经保存过该页面数据
-            const hasExist = await this.#find(id)
-            if (hasExist) {
+            const queryData = await this.#find(id)
+            if (queryData && await this.itemCompleteCheck(queryData)) {
                 this.log.warn(`item data has saved : ${id}`)
                 continue
             }
+
             try {
                 const itemData = await this.getItemData(item)
                 const contentTest = JSON.stringify(itemData)
@@ -535,6 +536,10 @@ class ListJob extends Job {
         }
     }
     async getItemId(item) {
+        if(this.conf.CustomStage?.GetItemId){
+            return this.custom.getItemId.call(this,item)
+        }
+
         let id = ''
         if(Array.isArray(item)){
             const itemData = []
@@ -580,7 +585,12 @@ class ListJob extends Job {
         }
         return id
     }
-
+    async itemCompleteCheck(item){
+        if(this.conf.CustomStage?.ItemCompleteCheck){
+            return this.custom.itemCompleteCheck.call(this,item)
+        }
+        return true
+    }
     getItemSelector() {
         const locValue = this.processConfig.GetItemData?.selector?.value
         let selector = this.driver.buildSelectorForXpath(locValue)
